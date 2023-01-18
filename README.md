54d342ad72e4ffd080268924be2388d5316b51d1
hannahsm1th <smith.hannah.rachel@gmail.com>
2023-01-18 15:49:42 +1100

    Game class - initialise
    Added error handling for loading the board.json file
    blank.json can be used to test this

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # woven_monopoly
 Coding test
 
-# Extensibility
+## Explanation
+
+### dice_roll()
+This method allows for testing using randomly generated rolls.
+
+### Player class
+Stores the data for the each player of the game.
+
+## Extensibility
 * Dice rolling uses function dice_roll() allowing to extend method for multiple rolls, different number of die sides, or rolling at advantage or disadvantage (rolling two die and selecting the higher or lower of the two results)
-* The Player class could methods to simulate player behaviour, such as making choices about gameplay
+* The Player class could methods to simulate player behaviour, such as making choices about gameplay. The data here could be displayed to each user.
--- monopoly.py
+++ monopoly.py
@@ -4,6 +4,7 @@
 
 # Imports
 import random
+import json
 
 
 # Methods
@@ -20,16 +21,43 @@ def dice_roll(sides = 6):
 # Classes
 
 class Player():
+
     """
     Class for the game players
     All players start with $16 and no properties
     """
+
     def __init__(self, name, money = 16, properties = []):
         self.name = name
         self.money = money
         self.properties = properties
 
+
+class Game():
+
+    """
+    Class for the game logic, simulating the players movements around the board.
+    The game board is created from a given path to a JSON file
+    """
+
+    def __init__(self, board_path, players):
+        try:
+            if type(board_path) is not str:
+                raise Exception("Board path should be a string giving the path to the JSON file")
+            with open(board_path, 'r') as f:
+                self.board = json.load(f)
+            if len(self.board) < 1:
+                raise Exception("Given JSON file is empty")
+        except json.decoder.JSONDecodeError:
+            print("Cannot decode given JSON file")
+        except FileNotFoundError:
+            print("Board path should give the path to a JSON file")
+        self.players = players
+
+
 # Game setup
 
 names = ['Peter', 'Billy', 'Charlotte', 'Sweedal']
 players = [Player(name) for name in names]
+
+game = Game('board.json', players)
